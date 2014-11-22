55d15797b287f1f1dfce004d9edea21cc224fff4
Darshan Patil <darshanpatil123@yahoo.com>
2014-11-22 09:58:22 -0800

    hopefully fixed IO

--- src/SVMLearn.java
+++ src/SVMLearn.java
@@ -5,10 +5,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.util.*;
 import libsvm.*;
 
@@ -24,11 +27,12 @@ public class SVMLearn{
 		conf.setJobName("wordcount");
 		conf.setOutputKeyClass(IntWritable.class);
 		conf.setOutputValueClass(Text.class);
-		
+		conf.setInt(NLineInputFormat.LINES_PER_MAP, 200000);
 		conf.setMapperClass(Map.class);
 	    conf.setCombinerClass(Reduce.class);
 	    conf.setReducerClass(Reduce.class);
-	    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+	    conf.setInputFormat(InpFormat.class);
+	    FileInputFormat.setInputPaths(conf, args[0]);
 	    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 	    JobClient.runJob(conf);
 
@@ -76,6 +80,125 @@ public class SVMLearn{
 			arg2.collect(arg0, new Text(s.toString()));
 		}
 	 }
+	public class InpFormat extends FileInputFormat<IntWritable, Text> {
+
+		@Override
+		public RecordReader<IntWritable, Text> getRecordReader(InputSplit input,
+				JobConf job, Reporter reporter) throws IOException {
+			// TODO Auto-generated method stub
+			reporter.setStatus(input.toString());
+		    return new InpRecordReader(job, (FileSplit)input);
+		}
+	}
+	static int block = 0;
+
+	public class InpRecordReader implements RecordReader<IntWritable, Text>{
+		private final int NLINESTOPROCESS = 200000;
+		private LineReader in;
+		private IntWritable key;
+		private Text value = new Text();
+		private long start =0;
+		private long end =0;
+		private long pos =0;
+		private int maxLineLength;
+		public InpRecordReader(JobConf job, FileSplit split)throws IOException{
+	        final Path file = split.getPath();
+	        
+	        this.maxLineLength = job.getInt("mapred.linerecordreader.maxlength",Integer.MAX_VALUE);
+	        FileSystem fs = file.getFileSystem(job);
+	        start = split.getStart();
+	        end= start + split.getLength();
+	        boolean skipFirstLine = false;
+	        FSDataInputStream filein = fs.open(split.getPath());
 	 
+	        if (start != 0){
+	            skipFirstLine = true;
+	            --start;
+	            filein.seek(start);
+	        }
+	        in = new LineReader(filein,job);
+	        if(skipFirstLine){
+	            start += in.readLine(new Text(),0,(int)Math.min((long)Integer.MAX_VALUE, end - start));
+	        }
+	        this.pos = start;
+		}
+		@Override
+		public void close() throws IOException {
+			// TODO Auto-generated method stub
+			if (in != null) {
+	            in.close();
+	        }
+		}
+
+		@Override
+		public IntWritable createKey() {
+			// TODO Auto-generated method stub
+			block++;
+			return new IntWritable(block);
+		}
+
+		@Override
+		public Text createValue(){
+			// TODO Auto-generated method stub
+			if (key == null) {
+	            key = new IntWritable();
+	        }
+	        key.set(block);
+	        if (value == null) {
+	            value = new Text();
+	        }
+	        value.clear();
+	        final Text endline = new Text("\n");
+	        int newSize = 0;
+	        for(int i=0;i<NLINESTOPROCESS;i++){
+	            Text v = new Text();
+	            while (pos < end) {
+	                try {
+						newSize = in.readLine(v, maxLineLength,Math.max((int)Math.min(Integer.MAX_VALUE, end-pos),maxLineLength));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	                value.append(v.getBytes(),0, v.getLength());
+	                value.append(endline.getBytes(),0, endline.getLength());
+	                if (newSize == 0) {
+	                    break;
+	                }
+	                pos += newSize;
+	                if (newSize < maxLineLength) {
+	                    break;
+	                }
+	            }
+	        }
+	        if (newSize == 0) {
+	            key = null;
+	            value = null;
+	        }
+	        return value;
+		}
+
+		@Override
+		public long getPos() throws IOException {
+			// TODO Auto-generated method stub
+			return pos;
+		}
+
+		@Override
+		public float getProgress() throws IOException {
+			// TODO Auto-generated method stub
+			return (float) pos / end;
+		}
+
+		@Override
+		public boolean next(IntWritable arg0, Text arg1) throws IOException {
+			// TODO Auto-generated method stub
+			arg0=createKey();
+			arg1=createValue();
+			return true;
+		}
+		
+	}
+	
+	
 
 }
